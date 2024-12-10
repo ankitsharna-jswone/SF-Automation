f4e57a2dcc8a9c1af437fdf2aab8d4bb2f8409b1
Ankit Sharma <V_Ankit.Sharma@JSW.IN>
2024-12-11 01:41:00 +0530

    new yml file config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
-
+import java.lang.reflect.Array;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
+    public static void main(String[] args) {
 
 
 
+    }
 }
--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -4,22 +4,22 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = {"Stepdefination","AppHoocks"},
         plugin = {"html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
+                "json:target/cucumber.json",
                 "pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "html:test-output/html/HTMLreport.html",
                 "Listner.CustomCucumberListener"
         },
-        tags = "@newCreditTest"
+        tags = "@uatconst"
 
 )
-@RunWith(Cucumber.class)
 public class Runner {
 
 }
